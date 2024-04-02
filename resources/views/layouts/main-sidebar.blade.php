e2130e226ba40237dbed7b8d8109f60c700e477a
anas nashat <aanasht@gmail.com>
2024-04-02 04:00:23 +0200

    Add Messages

--- resources/views/layouts/main-sidebar.blade.php
+++ resources/views/layouts/main-sidebar.blade.php
@@ -2,7 +2,9 @@
 		<div class="app-sidebar__overlay" data-toggle="sidebar"></div>
 		<aside class="app-sidebar sidebar-scroll">
 			<div class="main-sidebar-header active">
-				<a class="desktop-logo logo-light active" href="{{ url('/' . $page='dashboard') }}"><img src="{{asset('assets/img/brand/logo.png')}}" class="main-logo" alt="logo"></a>
+                <a class="desktop-logo logo-light active" href="{{ url('/' . $page='dashboard') }}">
+                    <i class="fas fa-album side-menu__icon" aria-hidden="true"></i>
+                </a>
 				<a class="desktop-logo logo-dark active" href="{{ url('/' . $page='dashboard') }}"><img src="{{asset('assets/img/brand/logo-white.png')}}" class="main-logo dark-theme" alt="logo"></a>
 				<a class="logo-icon mobile-logo icon-light active" href="{{ url('/' . $page='dashboard') }}"><img src="{{asset('assets/img/brand/favicon.png')}}" class="logo-icon" alt="logo"></a>
 				<a class="logo-icon mobile-logo icon-dark active" href="{{ url('/' . $page='dashboard') }}"><img src="{{asset('assets/img/brand/favicon-white.png')}}" class="logo-icon dark-theme" alt="logo"></a>
@@ -11,7 +13,7 @@
 				<div class="app-sidebar__user clearfix">
 					<div class="dropdown user-pro-body">
 						<div class="">
-							<img alt="user-img" class="avatar avatar-xl brround" src="{{asset('assets/img/faces/6.jpg')}}"><span class="avatar-status profile-status bg-green"></span>
+							<img alt="user-img" class="avatar avatar-xl brround" src="https://cdn-icons-png.flaticon.com/512/9131/9131529.png"><span class="avatar-status profile-status bg-green"></span>
 						</div>
 						<div class="user-info">
 							<h4 class="font-weight-semibold mt-3 mb-0">{{auth()->user()->name}}</h4>
@@ -23,12 +25,14 @@
 
 
 					<li class="slide">
-						<a class="side-menu__item" data-toggle="slide" href="{{ route('albums.index') }}"><svg xmlns="http://www.w3.org/2000/svg" class="side-menu__icon" viewBox="0 0 24 24"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M19 5H5v14h14V5zM9 17H7v-7h2v7zm4 0h-2V7h2v10zm4 0h-2v-4h2v4z" opacity=".3"/><path d="M3 5v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2H5c-1.1 0-2 .9-2 2zm2 0h14v14H5V5zm2 5h2v7H7zm4-3h2v10h-2zm4 6h2v4h-2z"/></svg><span class="side-menu__label">الفواتير</span><i class="angle fe fe-chevron-down"></i></a>
+                        <a class="side-menu__item" data-toggle="slide" href="{{ route('albums.index') }}">
+                            <i class="fa fa-image side-menu__icon" aria-hidden="true"></i>
+                            <span class="side-menu__label">Albums</span>
+                            <i class="angle fe fe-chevron-down"></i>
+                        </a>
 						<ul class="slide-menu">
-							<li><a class="slide-item" href="{{ route('albums.index') }}">قائمه الفواتير</a></li>
-							<li><a class="slide-item" href="{{ route('albums.index','filter[status]=1') }}">الفواتير المدفوعه</a></li>
-							<li><a class="slide-item" href="{{ route('albums.index','filter[status]=0')  }}">الفواتير الغير مدفوعه </a></li>
-							<li><a class="slide-item" href="{{ route('albums.index','filter[status]=2')  }}">الفواتير المدفوعه جزئيا</a></li>
+							<li><a class="slide-item" href="{{ route('albums.index') }}">All Albums</a></li>
+
 						</ul>
 					</li>
 
